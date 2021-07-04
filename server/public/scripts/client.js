8e55b42629f85a759e94fdadc65ede528d793e4f
Kong Yang <kpyang2414@gmail.com>
2021-07-03 21:54:29 -0500

    working on ajax

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -2,4 +2,54 @@ console.log('js ready');
 
 $(document).ready( () => {
     console.log('JQ ready');
-})
+    createTaskListener();
+    getTaskList();
+    deleteTaskListener();
+
+});
+
+/**
+ * function will listen for click on create task
+ */
+function createTaskListener() {
+    console.log('made it to createTaskListner');
+    $('#create-button').on('click', () => {
+        console.log('create task button clicked');
+        let taskToSend = {
+            name: $('#task-name').val();
+            description: $('#description').val();
+            status: $('#status-checkbox').val();
+        };
+
+        addTask(taskToSend);
+
+    });
+}
+
+/**
+ * params: taskToSend
+ */
+function addTask(taskToSend) {
+    console.log('made it to function addTask');
+    $.ajax({
+        method: 'POST',
+        url: '/weekendApp',
+        data: taskToSend
+    })
+    .then((response) => {
+        console.log('back at ');
+        getTaskList();
+    })
+    .catch((error) => {
+        console.log('task did not send' error);
+    })
+};
+
+
+
+/**
+ * Will refresh the tasklist
+ */
+ function getTaskList();
+
+function deleteTask();
